eb27226858cccf9fc703c4a272ab9d7540f83b3e
Ryan Nixon <r.taiidani@gmail.com>
2024-05-18 12:29:01 -0700

    URL, DTSTAMP additions

--- internal/event-calendar.go
+++ internal/event-calendar.go
@@ -15,6 +15,9 @@ import (
 
 type charlemagneEvent struct {
 	ID          string
+	URL         string
+	GuildID     string
+	ChannelID   string
 	Activity    string
 	StartTime   time.Time
 	Description string
@@ -55,6 +58,11 @@ func eventCalendarHandler(s *discordgo.Session, i *discordgo.InteractionCreate) 
 			continue
 		}
 
+		// GuildID is not always populated 🤷
+		if msg.GuildID == "" {
+			msg.GuildID = i.GuildID
+		}
+
 		evt, err := parseEvent(ctx, i.Member, *msg)
 		if err != nil {
 			slog.Warn("Unable to parse message", "author", msg.Author.Username)
@@ -87,13 +95,22 @@ func eventCalendarHandler(s *discordgo.Session, i *discordgo.InteractionCreate) 
 }
 
 func parseEvent(_ context.Context, _ *discordgo.Member, msg discordgo.Message) (charlemagneEvent, error) {
-	ret := charlemagneEvent{ID: msg.ID}
+	ret := charlemagneEvent{
+		ID:        msg.ID,
+		ChannelID: msg.ChannelID,
+		GuildID:   msg.GuildID,
+	}
 
 	if len(msg.Embeds) == 0 {
 		return ret, fmt.Errorf("message did not contain any embeds")
 	}
 	if len(msg.Embeds[0].Fields) == 0 {
 		return ret, fmt.Errorf("message embed did not contain any fields")
+	}
+
+	if ret.GuildID != "" && ret.ChannelID != "" && ret.ID != "" {
+		// Discord links are https://discord.com/channels/372591705754566656/1241174079030169641/1241294928340844601
+		ret.URL = fmt.Sprintf("https://discord.com/channels/%s/%s/%s", ret.GuildID, ret.ChannelID, ret.ID)
 	}
 
 	for _, f := range msg.Embeds[0].Fields {
@@ -142,35 +159,67 @@ func eventCalendarResponse(events []charlemagneEvent) (*discordgo.InteractionRes
 		return ret, fmt.Errorf("no events were found")
 	}
 
-	content := strings.Builder{}
 	for _, evt := range events {
-		content.WriteString(fmt.Sprintf("> %s. Join ID: %s\n", evt.Activity, evt.JoinID))
-		content.WriteString(fmt.Sprintf("> <t:%d:F>\n", evt.StartTime.Unix()))
-		content.WriteString(fmt.Sprintf("> Guardians: %s\n\n", strings.Join(evt.Guardians, ", ")))
+		description := "Event information parsed:"
+		if evt.URL != "" {
+			description = "Event information parsed for " + evt.URL + ":"
+		}
+
+		ret.Embeds = append(ret.Embeds, &discordgo.MessageEmbed{
+			Description: description,
+			Color:       defaultColor,
+			Footer:      &discordgo.MessageEmbedFooter{Text: "Event ID: " + evt.JoinID},
+			Fields: []*discordgo.MessageEmbedField{
+				{
+					Name:   "Activity",
+					Value:  evt.Activity,
+					Inline: true,
+				},
+				{
+					Name:   "Start Time",
+					Value:  fmt.Sprintf("<t:%d:F>\n", evt.StartTime.Unix()),
+					Inline: true,
+				},
+				{
+					Name:   "Description",
+					Value:  evt.Description,
+					Inline: false,
+				},
+				{
+					Name:   "Guardians",
+					Value:  strings.Join(evt.Guardians, ", "),
+					Inline: false,
+				},
+			},
+		})
 
 		ret.Files = append(ret.Files, &discordgo.File{
-			Name:        fmt.Sprintf("D2-%s-%s.ics", evt.Activity, evt.ID),
+			Name:        fmt.Sprintf("D2-%s-%s-%s.ics", evt.Activity, evt.GuildID, evt.JoinID),
 			ContentType: "text/calendar",
 			Reader:      buildCalendarEvent(evt),
 		})
 	}
 
-	ret.Content = content.String()
 	return ret, nil
 }
 
 func buildCalendarEvent(evt charlemagneEvent) *strings.Reader {
-	// For reference: 2006-01-02T15:04:05Z07:00
+	// Go date format for reference: 2006-01-02T15:04:05Z07:00
 	const dtFormat = "20060102T150405Z"
 	evt.StartTime = evt.StartTime.In(time.UTC)
 	dtEnd := evt.StartTime.Add(time.Hour * 2)
+	dtStamp := time.Now().In(time.UTC)
 
 	ical := strings.Builder{}
 	ical.WriteString("BEGIN:VCALENDAR\n")
 	ical.WriteString("VERSION:2.0\n")
 	ical.WriteString("PRODID:-//hacksw/handcal//NONSGML v1.0//EN\n")
 	ical.WriteString("BEGIN:VEVENT\n")
-	ical.WriteString("UID:" + evt.ID + "\n")
+	ical.WriteString(fmt.Sprintf("UID:%s-%s\n", evt.GuildID, evt.JoinID))
+	if evt.URL != "" {
+		ical.WriteString("URL:" + evt.URL + "\n")
+	}
+	ical.WriteString("DTSTAMP:" + dtStamp.Format(dtFormat) + "\n")
 	ical.WriteString("DTSTART:" + evt.StartTime.Format(dtFormat) + "\n")
 	ical.WriteString("DTEND:" + dtEnd.Format(dtFormat) + "\n")
 	ical.WriteString("SUMMARY:" + evt.Activity + "\n")
