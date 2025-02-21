1e7b79daef78e1dbfe7db92ed128ae89f897536d
Ryan Nixon <r.taiidani@gmail.com>
2025-02-21 01:39:21 -0800

    Pushing the OAuth wall a little further

--- internal/models/session.go
+++ internal/models/session.go
@@ -1,8 +1,21 @@
 package models
 
+import (
+	"time"
+)
+
 type Session struct {
-	DiscordID  string
-	Name       string
-	ProfileURL string
-	AvatarURL  string
+	Auth        *DiscordAuth
+	DiscordUser *DiscordUser
+}
+
+type DiscordUser struct {
+	ID       string
+	Username string
+}
+
+type DiscordAuth struct {
+	AccessToken  string
+	ExpiresAt    time.Time
+	RefreshToken string
 }
